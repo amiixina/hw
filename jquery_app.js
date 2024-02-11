51b44a9f3225d681f6f301d109255940430d52af
Амина Задорожная <aminazadoroznaa@MacBook-Pro-Amina.local>
2024-02-11 16:02:31 +0200

    u

--- jquery_app.js
+++ jquery_app.js
@@ -1,11 +1,14 @@
 $(document).ready(
     function (){
-    $("#addHtmlt").html('<h1> New Element </h1>')
+// -------- 1 --------
+$("#addHtmlt").html('<h1> New Element </h1>');
+// -------- 2 --------
     $(".a").hide();
+// -------- 3 --------
     $("section").remove();
-    $("div").text("Lumos Maxima");
-
-    $("div.a").css({
+// -------- 4 ---------
+    $("div.b").text("Lumos Maxima");
+    $("div.b").css({
         background: "#f66668",
         borderRadius: "50% 0 50% 0",
         width: "200px",
