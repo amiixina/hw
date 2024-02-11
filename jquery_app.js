42cab746edcc582be31c1a34ce9217dc8732c9e4
Амина Задорожная <aminazadoroznaa@MacBook-Pro-Amina.local>
2024-02-11 14:36:29 +0200

    q

--- jquery_app.js
+++ jquery_app.js
@@ -1,17 +1,2 @@
-$(document).ready(function() {
-    $("#addHtmlt").html('<h1> New Element </h1>')
-    $(".a").hide();
-    $("section").remove();
-    $("div").addClass(function () {
-        $(this).text("Lumos Maxima");
-    });
 
-    $("div.a").css({
-        background: "#f66668",
-        borderRadius: "50% 0 50% 0",
-        width: "200px",
-        height: "120px",
-        lineHeight: "80px",
-        textAlign: "center",
-    });
-});
+
