0464e05897cade191e085a2ec76d040a9c58156a
Амина Задорожная <aminazadoroznaa@MacBook-Pro-Amina.local>
2024-02-11 14:42:23 +0200

    q

--- jquery_app.js
+++ jquery_app.js
@@ -1,2 +1,17 @@
+$( function (){
+console. log("Document READY");
+});
+    $("#addHtmlt").html('<h1> New Element </h1>')
+    $(".a").hide();
+    $("section").remove();
+    $("div").text("Lumos Maxima");
 
+    $("div.a").css({
+        background: "#f66668",
+        borderRadius: "50% 0 50% 0",
+        width: "200px",
+        height: "120px",
+        lineHeight: "80px",
+        textAlign: "center",
+    });
 
